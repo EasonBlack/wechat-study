709b0edebb4c42b026f430557ecf5b1c8771375d
EasonBlack <eason820720@163.com>
2018-10-21 11:11:37 +0800

    up

---
+++ basic/test-web-view/pages/view1/view1.js
@@ -0,0 +1,22 @@
+
+Page({
+  data: {
+    
+  },
+
+  onLoad:  function() {
+    // console.log('load')
+    // wx.setNavigationBarTitle({
+    //   title: 'Account Center'
+    // });
+
+    // wx.setNavigationBarColor({
+    //   backgroundColor: '#594F60',
+    //   frontColor: "#ffffff"
+    // });
+  },
+
+  test(e) {
+    console.log(e)
+  }
+})
